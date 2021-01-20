b6ece9397884b725136f429c585bef0b12c8048d
Bleak Chandler <bleakchandler@gmail.com>
2021-01-20 23:39:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,7 @@ def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   hash[key]
 hash = {
-  a: 1, b: 2
+  a: key
 }
 end
 
@@ -48,4 +48,4 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-end
+laern
